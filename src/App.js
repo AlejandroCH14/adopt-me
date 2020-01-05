3a0da8d1a4b0c817673125a4a8045fe3ab0e0d74
Punisher <abi.ft7@gmail.com>
2020-01-04 20:18:40 -0600

    Added SearchParams.js and began using hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Kelly",
-  //     animal: "Dog",
-  //     breed: "Poodle"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed"
-  //   })
-  // ]);
-
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Kelly" animal="Dog" breed="Poodle" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
